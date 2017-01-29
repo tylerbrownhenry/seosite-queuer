964e007e38a4be347d7ea338d2cecbd685fa7015
Tyler Henry <tyler@milmerry.com>
2017-01-29 13:58:24 -0500

    test

--- server.js
+++ server.js
@@ -49,7 +49,12 @@ var permissions = {
     app.use(morgan('dev'));
 
     var apiRoutes = express.Router(); 
-
+    app.all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+        next();
+    });
     // ---------------------------------------------------------
     // authenticated routes
     // ---------------------------------------------------------
