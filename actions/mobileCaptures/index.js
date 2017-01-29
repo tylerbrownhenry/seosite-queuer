41940a35958a547eca5f5744c0d3743913721db0
Tyler Henry <tyler@milmerry.com>
2017-01-29 18:27:23 -0500

    ‘test’

--- actions/mobileCaptures/index.js
+++ actions/mobileCaptures/index.js
@@ -49,8 +49,7 @@ function doit(url,requestId){
                 });
             }).then(function(){
                 console.log('done 2');
-
-                
+                ph.exit();
             }).finally(function(){
                 console.log('done');
             })
